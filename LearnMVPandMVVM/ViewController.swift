60fd7b23a6a4aba00a28f8473ca68b0cc73ec631
Ly Chenghav <lychenghavsloy@gmail.com>
2023-09-29 13:34:18 +0700

    Initial Commit

---
+++ LearnMVPandMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnMVPandMVVM
+//
+//  Created by Ly Chenghav on 29/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
